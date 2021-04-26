ab5f2db5edcd5b0051159228887d663a5e727d3d
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-04-26 01:10:23 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 console.log('%c HI', 'color: firebrick')
 
-window.addEventListener("DOMContentLoad", ()=>{
+window.addEventListener("DOMContentLoaded", ()=>{
   const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
   fetch(imgUrl)
   .then(resp => resp.json())
