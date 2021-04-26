777cb95c52b57658d6e7c5c2e4521efa32c10b39
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-04-26 01:18:26 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 console.log('%c HI', 'color: firebrick');
-let breeds =[]
+let breeds =[];
 
 window.addEventListener("DOMContentLoaded", ()=>{
   const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
