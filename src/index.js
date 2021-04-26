4fc44f985b8e0f9936352ba70667dbe7ebc9c772
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-04-26 01:10:09 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 console.log('%c HI', 'color: firebrick')
 
 window.addEventListener("DOMContentLoad", ()=>{
-  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
   fetch(imgUrl)
   .then(resp => resp.json())
   .then(json => {json.message.forEach(image => add(image))})
