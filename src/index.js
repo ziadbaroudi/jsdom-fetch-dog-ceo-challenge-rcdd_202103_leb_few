69660e80ac0832d39c0c9c153cf7b836388224b0
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-04-26 01:09:48 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ window.addEventListener("DOMContentLoad", ()=>{
   const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
   fetch(imgUrl)
   .then(resp => resp.json())
-  .then(json => json)
+  .then(json => {json.message.forEach(image => add(image))})
   
-  function add(){
+  function add(url){
     let div = document.getElementById("dog-image-container");
     let im = document.createElement("img");
     im.src = url;
